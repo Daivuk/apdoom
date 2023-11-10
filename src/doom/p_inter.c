dc75769d74e933d2d0e460fe7d73e1d554a9a85f
coavins <me@coavins.org>
2023-11-09 22:12:01 -0500

    Fix whitespace

--- src/doom/p_inter.c
+++ src/doom/p_inter.c
@@ -641,20 +641,21 @@ P_TouchSpecialThing
 	break;
 	
       case SPR_BPAK:
-        for (i=0 ; i<NUMAMMO ; i++)
-        {
-					// If we can, upgrade max ammo
-          if( player->backpacks[i] < player->maxbackpacks[i] )
-					{
-            player->backpacks[i] += 1;
-            player->maxammo[i] = player->maxammo_initial[i] +
-              (player->maxammo_increment_linear[i] * player->backpacks[i]);
-          }
-					// Either way, give ammo
-          P_GiveAmmo (player, i, 1, false);
-				}
-        player->message = DEH_String(GOTBACKPACK);
-        break;
+	for (i=0 ; i<NUMAMMO ; i++)
+	{
+		// If we can, upgrade max ammo
+		if( player->backpacks[i] < player->maxbackpacks[i] )
+		{
+			player->backpacks[i] += 1;
+			player->maxammo[i] = player->maxammo_initial[i] +
+				(player->maxammo_increment_linear[i]
+					* player->backpacks[i]);
+		}
+		// Either way, give ammo
+		P_GiveAmmo (player, i, 1, false);
+	}
+	player->message = DEH_String(GOTBACKPACK);
+	break;
 	
 	// weapons
 	// [NS] Give half ammo for all dropped weapons.
